9038abf6ae569303222c976e9d8105bd0963a919
zyh <1796172146@qq.com>
2022-07-24 19:13:06 +0800

    Idea配置github测试6(DEMO)

--- src/main/java/gitdemo/gitDemo.java
+++ src/main/java/gitdemo/gitDemo.java
@@ -6,5 +6,7 @@ public class gitDemo {
         System.out.println("hello!git2!");
         System.out.println("hello!git3!");
         System.out.println("hello!git4!");
+        System.out.println("hello!git5!");
+        System.out.println("hello!git6!");
     }
 }
