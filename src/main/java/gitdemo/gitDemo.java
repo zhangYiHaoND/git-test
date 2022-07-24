a34583c87a5163de2ee00732b101ced552973a3a
zyh <1796172146@qq.com>
2022-07-24 19:03:05 +0800

    Idea配置github测试2

--- src/main/java/gitdemo/gitDemo.java
+++ src/main/java/gitdemo/gitDemo.java
@@ -2,6 +2,7 @@ package src.main.java.gitdemo;
 
 public class gitDemo {
     public static void main(String[] args) {
-        System.out.println("hello!world!");
+        System.out.println("hello!git!");
+        System.out.println("hello!git!");
     }
 }
