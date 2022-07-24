c218181a34354b32dc45a33abfcb6920abdb5452
zyh <1796172146@qq.com>
2022-07-24 19:36:13 +0800

    PUSHTEST

--- src/main/java/gitdemo/gitDemo.java
+++ src/main/java/gitdemo/gitDemo.java
@@ -10,5 +10,6 @@ public class gitDemo {
         System.out.println("hello!git7!");
         System.out.println("hello!git5!");
         System.out.println("hello!git6!");
+        System.out.println("pushtest");
     }
 }
